463b492c93f2efd73cccb2b9e99a94803e09a28f
Kyra Patton <kyra.patton@adadevelopersacademy.org>
2022-08-30 10:20:51 -0700

    Add time and space complexity

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -7,8 +7,8 @@ class Node:
 def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n+m)
+        Space Complexity: O(1)
     """
     l1, l2 = headA, headB
     while l1 != l2:
